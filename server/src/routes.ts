0d24d167828f4fc536712a9a3be1a071fc5f664d
Everton Fraga <evertonfraga@MacBook-Pro-de-Everton.local>
2020-06-12 15:45:36 -0300

    commit modulo avaçado

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,16 @@
 import express from 'express';
+import multer from 'multer';
+import multerconfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const upload = multer(multerconfig);
 
 routes.get('/items', new ItemsController().index);
 
-routes.post('/points', new PointsController().create);
+routes.post('/points', upload.single('image'), new PointsController().create);
 routes.get('/points/', new PointsController().index);
 routes.get('/points/:id', new PointsController().show);
 
